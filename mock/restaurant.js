e0557b94dbe337f5def269d25c521b31c7e35148
Joe Leland <joe@stackoverjoe.com>
2020-11-19 10:13:13 -0800

    Add mock data

---
+++ mock/restaurant.js
@@ -0,0 +1,114 @@
+export const restaurants = [
+  {
+    name: "Yum Yum",
+    description: "So good tbh",
+    address: {
+      street: "1234 the street",
+      city: "Portland",
+      state: "OR",
+      zip: 97214,
+    },
+    location: {
+      longitude: 107.12414,
+      latitude: 205.21515,
+    },
+    menu: [
+      {
+        name: "food name",
+        description: "food desc",
+        price: 20.5,
+        picURL: "someurl",
+      },
+      {
+        name: "food name 2",
+        description: "food desc 2",
+        price: 20.52,
+      },
+    ],
+    picURL: "link-to-restaurant photo",
+  },
+  {
+    name: "Other place",
+    description: "Not so good tbh",
+    address: {
+      street: "1234 the street",
+      city: "Portland",
+      state: "OR",
+      zip: 97214,
+    },
+    location: {
+      longitude: 101.12444,
+      latitude: 198.15672,
+    },
+    menu: [
+      {
+        name: "food name",
+        description: "food desc",
+        price: 20.5,
+        picURL: "someurl",
+      },
+      {
+        name: "food name 2",
+        description: "food desc 2",
+        price: 20.52,
+      },
+    ],
+    picURL: "link-to-restaurant photo",
+  },
+  {
+    name: "Other place",
+    description: "Not so good tbh",
+    address: {
+      street: "1234 the street",
+      city: "Portland",
+      state: "OR",
+      zip: 97214,
+    },
+    location: {
+      longitude: 101.12444,
+      latitude: 198.15672,
+    },
+    menu: [
+      {
+        name: "food name",
+        description: "food desc",
+        price: 20.5,
+        picURL: "someurl",
+      },
+      {
+        name: "food name 2",
+        description: "food desc 2",
+        price: 20.52,
+      },
+    ],
+    picURL: "link-to-restaurant photo",
+  },
+  {
+    name: "Other place",
+    description: "Not so good tbh",
+    address: {
+      street: "1234 the street",
+      city: "Portland",
+      state: "OR",
+      zip: 97214,
+    },
+    location: {
+      longitude: 101.12444,
+      latitude: 198.15672,
+    },
+    menu: [
+      {
+        name: "food name",
+        description: "food desc",
+        price: 20.5,
+        picURL: "someurl",
+      },
+      {
+        name: "food name 2",
+        description: "food desc 2",
+        price: 20.52,
+      },
+    ],
+    picURL: "link-to-restaurant photo",
+  },
+];
