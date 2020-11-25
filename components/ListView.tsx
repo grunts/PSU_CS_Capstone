8b016031a7499883b497cd7d1921382c5615e999
eleven011 <vkillian@pdx.edu>
2020-11-25 10:59:31 -0800

    dummy card

---
+++ components/ListView.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+//import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
+import { Button, Card, Title, Paragraph } from 'react-native-paper';
+//const restaurants = require("../mock/restaurant.js");
+
+const ListComponent = () => (
+    <Card>
+      <Card.Title title="Card Title" subtitle="Card Subtitle" />
+      <Card.Content>
+        <Title>Card title</Title>
+        <Paragraph>Card content</Paragraph>
+      </Card.Content>
+      <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+    </Card>
+  );
+
+  export default ListComponent;
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,20 +1,21 @@
 import * as React from "react";
 import { StyleSheet } from "react-native";
 
-import EditScreenInfo from "../components/EditScreenInfo";
+//import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
-const restaurants = require("../mock/restaurant");
+import ListComponent from "../components/ListView";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>List View</Text>
+      <ListComponent />
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <EditScreenInfo path="/screens/TabOneScreen.js" />
+      
     </View>
   );
 }
