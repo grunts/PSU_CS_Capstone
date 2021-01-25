59f592076a7cd19962ac12e589311c22262438dc
Jason Forral <Jason.Forral@gmail.com>
2021-01-25 13:51:13 -0800

    update menuscreen in progress

--- screens/MenuScreen.tsx
+++ screens/MenuScreen.tsx
@@ -6,13 +6,21 @@ import MenuItemComponent from "../components/MenuItemComponent";
 import DefaultRestaurant from "../constants/DefaultRestaurant";
 
 export default function MenuScreen({ route }) {
-  const { restaurants } = route.params;
-  const categories = extractCategories(restaurants);
+  const { restaurant } = route.params;
+  const { menu } = restaurant;
+  const categories = extractCategories(menu);
   return (
     <View style={styles.container}>
-        <MenuTitleComponent title={restaurants.name ?? DefaultRestaurant.name}></MenuTitleComponent>
+        <MenuTitleComponent title={restaurant.name ?? DefaultRestaurant.name}></MenuTitleComponent>
         {/*<MenuItemComponent menuItem={restaurant.menu[0]} />*/}
-        <SectionList sections={}/>
+        <SectionList
+          sections={categories}
+          keyExtractor={(item, index) => item + index}
+          renderItem={({ item }) => <MenuItemComponent menuItem={item} />}
+          renderSectionHeader={({ section: { title } }) => (
+            <Text>{title}</Text>
+          )}
+        />
     </View>
   );
 }
@@ -33,6 +41,7 @@ const styles = StyleSheet.create({
   },
 });
 
-function extractCategories(restaurants) {
-  
+const extractCategories = (menu: any) => {
+  const categoriesObj = menu.reduce((accumulator, currentItem) => accumulator[currentItem.category] ? accumulator[currentItem.category].push(currentItem) : accumulator[currentItem.category] = [currentItem], {})
+  return Object.keys(categoriesObj).map(category => ({ title: category, data: categoriesObj[category] }))
 }
