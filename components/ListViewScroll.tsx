ec8217f35603f4aa7a102ca9c3fb5b6fa390552f
Joe Leland <joeleland@JoeLeland0121.local>
2021-01-28 17:12:07 -0800

    Optimize imports and change useState to init with empty array rather than null

--- components/ListViewScroll.tsx
+++ components/ListViewScroll.tsx
@@ -1,59 +1,54 @@
-import React from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
-import restaurants from '../mock/restaurant.js';
-import { Avatar, ListItem } from 'react-native-elements';
-import SearchBar from './SearchBar';
-
-const Item = ({ name }) => (
-  <View style={styles.item}>
-    <Text style={styles.name}>{name}</Text>
-  </View>
-);
+import React from "react";
+import { SafeAreaView, FlatList, StyleSheet, StatusBar } from "react-native";
+import restaurants from "../mock/restaurant.js";
+import { Avatar, ListItem } from "react-native-elements";
+import SearchBar from "./SearchBar";
+import { useTheme } from "@react-navigation/native";
 
 const App = () => {
+  const { colors } = useTheme();
   const renderItem = ({ item }) => (
-  <ListItem bottomDivider>
-      <Avatar 
-        size="large"
-        source={{uri: item.picURL}} />
+    <ListItem bottomDivider>
+      <Avatar size="large" source={{ uri: item.picURL }} />
       <ListItem.Content>
         <ListItem.Title>{item.name}</ListItem.Title>
-        <ListItem.Subtitle>{item.address.street}, {item.address.city}, {item.address.state}, {item.address.zip}</ListItem.Subtitle>
+        <ListItem.Subtitle>
+          {item.address.street}, {item.address.city}, {item.address.state},{" "}
+          {item.address.zip}
+        </ListItem.Subtitle>
       </ListItem.Content>
     </ListItem>
   );
 
   return (
     <SafeAreaView style={styles.container}>
-      {/* <View style={styles.container}> */}
-        <SearchBar 
-          renderFunction = {renderItem} 
-          dataToBeSearched = {restaurants}
-          fieldToSearch = {"name"}
-        />
-      {/* </View> */}
-       <FlatList 
+      <SearchBar
+        renderFunction={renderItem}
+        dataToBeSearched={restaurants}
+        fieldToSearch={"name"}
+      />
+      <FlatList
         data={restaurants}
         renderItem={renderItem}
-        keyExtractor={item => item.description} />
+        keyExtractor={(item) => item.description}
+      />
     </SafeAreaView>
-    
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
-    height: '20%'
+    height: "20%",
   },
   container2: {
     flex: 4,
     marginTop: StatusBar.currentHeight || 0,
-    height: '20%'
+    height: "20%",
   },
   item: {
-    backgroundColor: '#f9c2ff',
+    backgroundColor: "#f9c2ff",
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,68 +1,89 @@
 // import React in our code
-import React, {useState, useEffect} from 'react';
+import React, { useState } from "react";
 
 // import all the components we are going to use
-import {
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  TextInput,
-  View,
-  FlatList
-} from 'react-native';
-import { useTheme } from '@react-navigation/native';
+import { Text, View, FlatList } from "react-native";
+import { useTheme } from "@react-navigation/native";
 import { SearchBar } from "react-native-elements";
-import { FontAwesome5 } from '@expo/vector-icons'; 
-import { AntDesign } from '@expo/vector-icons'; 
+import { FontAwesome5 } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 
 const DropdownSearch = (props) => {
-    const [search, setSearch] = useState('');
-    
-    const [filteredDataSource, setFilteredDataSource] = useState(null);
+  const [search, setSearch] = useState("");
 
-    let updateSearch = (search) => {
-      setSearch(search)
-      setFilteredDataSource(props.dataToBeSearched.filter((item) => item[props.fieldToSearch].toLowerCase().includes(search.toLowerCase())))
-    }
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
 
-    const { colors, dark } = useTheme();
-        return (
-            <>
-            <SearchBar 
-              placeholder = "Search..."
-              value={search}
-              onChangeText={updateSearch}
-              lightTheme={dark ? false : true}
-              inputStyle={!dark ? {color: 'black'} : {}}
-               />
-                <FlatList
-                data={search.length > 2 ? filteredDataSource : null}
-                keyExtractor={(item, index) => index.toString()}
-                initialNumToRender={1}
-                showsVerticalScrollIndicator={false}
-                renderItem={props.renderFunction}
-                contentContainerStyle={
-                  filteredDataSource && search.length > 2 && filteredDataSource.length ? {marginBottom: (50*filteredDataSource.length)} : {}
-                }
-                />
-            {
-              search.length > 2 && (filteredDataSource && filteredDataSource.length === 0) ?
-              <View style = {{height: 60, justifyContent: 'center', alignItems: 'center', margin: 5, paddingLeft: 5, paddingRight: 5}}>
-                <FontAwesome5 name="meh-blank" size={24} color={colors.text} />
-                <Text style={{color: colors.text, textAlign: 'center', fontWeight: '500', paddingTop: 2, paddingBottom: 2}}>Oops! Sorry, we didn't find anything that matched your search.</Text>
-                </View> :
-              null
-            }
+  let updateSearch = (search) => {
+    setSearch(search);
+    setFilteredDataSource(
+      props.dataToBeSearched.filter((item) =>
+        item[props.fieldToSearch].toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  };
 
-            {
-              search.length > 2 && (filteredDataSource && filteredDataSource.length) ?
-              <View style = {{height: 30, justifyContent: 'center', alignItems: 'center', backgroundColor: `${colors.background}`}}>
-                <AntDesign name="find" size={24} color={colors.text} />
-                </View> :
-              null
-            }
-        </> 
-        );
-}
+  const { colors, dark } = useTheme();
+  return (
+    <>
+      <SearchBar
+        placeholder="Search..."
+        value={search}
+        onChangeText={updateSearch}
+        lightTheme={dark ? false : true}
+        inputStyle={!dark ? { color: "black" } : {}}
+      />
+      <FlatList
+        data={search.length > 2 ? filteredDataSource : null}
+        keyExtractor={(item, index) => index.toString()}
+        initialNumToRender={1}
+        showsVerticalScrollIndicator={false}
+        renderItem={props.renderFunction}
+        contentContainerStyle={
+          search.length > 2 && filteredDataSource.length
+            ? { marginBottom: 50 * filteredDataSource.length }
+            : {}
+        }
+      />
+      {search.length > 2 && filteredDataSource.length === 0 ? (
+        <View
+          style={{
+            height: 60,
+            justifyContent: "center",
+            alignItems: "center",
+            margin: 5,
+            paddingLeft: 5,
+            paddingRight: 5,
+          }}
+        >
+          <FontAwesome5 name="meh-blank" size={24} color={colors.text} />
+          <Text
+            style={{
+              color: colors.text,
+              textAlign: "center",
+              fontWeight: "500",
+              paddingTop: 2,
+              paddingBottom: 2,
+            }}
+          >
+            Oops! Sorry, we didn't find anything that matched your search.
+          </Text>
+        </View>
+      ) : null}
+
+      {search.length > 2 && filteredDataSource.length ? (
+        <View
+          style={{
+            height: 30,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: `${colors.background}`,
+          }}
+        >
+          <AntDesign name="find" size={24} color={colors.text} />
+        </View>
+      ) : null}
+    </>
+  );
+};
 
 export default DropdownSearch;
