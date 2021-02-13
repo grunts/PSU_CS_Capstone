e9abcd7851c84785c8e28f221d41efd6cf45c16f
Joe Leland <joeleland@JoeLeland0121.local>
2021-02-12 17:49:16 -0800

    Possible style path for confirm button

--- screens/ServingTray.tsx
+++ screens/ServingTray.tsx
@@ -1,38 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, FlatList, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, FlatList, Button, TouchableOpacity } from "react-native";
 import { Text, View } from "../components/Themed";
 import MenuItemComponent from "../components/MenuItemComponent";
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { MenuItem } from '../types';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MenuItem } from "../types";
 import restaurants from "../mock/restaurant.js";
 import SearchBar from "../components/SearchBar";
 
-
-export default function ServingTray({navigation}: {navigation: any}) {
+export default function ServingTray({ navigation }: { navigation: any }) {
   const [tray, setTray] = useState(tempTray);
 
   const renderItem = ({ item, index }) => (
     <View>
       <MenuItemComponent menuItem={item}>
-          {/**Use a convenient button component from react-native-vector-icons to create an remove from tray button.*/}
-          <MaterialCommunityIcons.Button
-            name="tray-minus"
-            onPress={() => {setTray(removeItem(tray, index))}}
-            size={24}
-            color="#a28"
-            backgroundColor="white"
-            accessibilityLabel="Remove item from tray">
-              Remove
-          </MaterialCommunityIcons.Button>
+        {/**Use a convenient button component from react-native-vector-icons to create an remove from tray button.*/}
+        <MaterialCommunityIcons.Button
+          name="tray-minus"
+          onPress={() => {
+            setTray(removeItem(tray, index));
+          }}
+          size={24}
+          color="#a28"
+          backgroundColor="white"
+          accessibilityLabel="Remove item from tray"
+        >
+          Remove
+        </MaterialCommunityIcons.Button>
       </MenuItemComponent>
     </View>
-  )
+  );
 
   // Returns a copy of the tray with indexed item removed
-  const removeItem = (array : MenuItem[], index : number) => [ ...array.slice(0, index), ...array.slice(++index)]
+  const removeItem = (array: MenuItem[], index: number) => [
+    ...array.slice(0, index),
+    ...array.slice(++index),
+  ];
 
   // Reducer that sums the price of all the serving tray items
-  const total = tray.reduce((accumulator : number, currentItem : MenuItem) => accumulator += currentItem.price, 0)
+  const total = tray.reduce(
+    (accumulator: number, currentItem: MenuItem) =>
+      (accumulator += currentItem.price),
+    0
+  );
 
   return (
     <View style={styles.container}>
@@ -45,23 +54,25 @@ export default function ServingTray({navigation}: {navigation: any}) {
         data={tray}
         renderItem={renderItem}
         keyExtractor={(item: MenuItem, index: number) => item.name + index}
+        ListFooterComponent={<View style={{padding: 40}}></View>}
       />
-      <View>
-        <Text>Total: {MakeCurrencyString(total)}</Text>
-        <Button
-          onPress={() => {}}
-          title="Confirm"
-          color="#a28"
-          accessibilityLabel="Confirm total purchase"
-        />
-      </View>
+      {/* <Text>Total: {MakeCurrencyString(total)}</Text> */}
+      <TouchableOpacity
+        onPress={() => {}}
+        accessibilityLabel="Confirm total purchase"
+        style={styles.confirmButton}
+      >
+        <Text style={{ color: "white" }}>
+          Confirm - {MakeCurrencyString(total)}
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const MakeCurrencyString = ( value : number ) => {
-  return `$${value.toFixed(2)}`
-}
+const MakeCurrencyString = (value: number) => {
+  return `$${value.toFixed(2)}`;
+};
 
 // const formatter = new Intl.NumberFormat('en-US', {
 //   style: 'currency',
@@ -75,14 +86,13 @@ const tempReducer = (accumulator, currentItem) => {
   return accumulator;
 };
 
-
-const tempTray = restaurants.reduce(tempReducer, [])
-
+const tempTray = restaurants.reduce(tempReducer, []);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    backgroundColor: "transparent",
   },
   title: {
     fontSize: 20,
@@ -92,5 +102,19 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 10,
     width: "80%",
+  },
+  confirmButton: {
+    backgroundColor: "#a28",
+    padding: 15,
+    marginLeft: 60,
+    marginRight: 60,
+    marginBottom: 15,
+    marginTop: 15,
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    left: 50,
+    bottom: 5,
   },
 });
